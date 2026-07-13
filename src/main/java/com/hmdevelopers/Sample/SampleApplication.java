539e5d8e1197e09dcf4130bd9c83319d667c747f
muhammadhasnainmughal <muhammadhasnain.bscsf23@iba-suk.edu.pk>
2026-07-13 16:08:59 +0500

    Video 1 end Foundation to SpringBoot

--- src/main/java/com/hmdevelopers/Sample/SampleApplication.java
+++ src/main/java/com/hmdevelopers/Sample/SampleApplication.java
@@ -1,13 +1,18 @@
 package com.hmdevelopers.Sample;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleApplication {
+public class SampleApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+
 	}
 
 }
